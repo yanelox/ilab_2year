09fa1e66d07430e5391a1067af11a737ddd3e060
yanelox <hokeygo@gmail.com>
2021-09-21 21:57:36 +0300

    add main code to main.cpp

--- Triangle/main.cpp
+++ Triangle/main.cpp
@@ -4,5 +4,33 @@
 
 int main ()
 {
-    ;
+    int n;
+
+    double* data;
+    triangle_* t;
+
+    cin >> n;
+
+    data = new double [9];
+    t = new triangle_ [n];
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+            cin >> data[j];
+
+        t[i] = {{data[0], data[1], data[2]},
+                {data[3], data[4], data[5]},
+                {data[6], data[7], data[8]}};
+    }
+
+    for (int i = 0; i < n; ++i)
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (t[i].trl_intersect (t[j]))
+                cout << i << " " << j << endl;
+        }
+
+    delete[] data;
+    delete[] t;
 }
