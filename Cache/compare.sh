af859c0d961e6c17456c8d12130c33b93d266ca9
yanelox <hokeygo@gmail.com>
2021-10-10 20:04:08 +0300

    Minor changes in "compare.sh". Now script print count of hits for both cache's

--- Cache/compare.sh
+++ Cache/compare.sh
@@ -6,8 +6,8 @@ arc_answ=$(cat ARC/answ/$i.answ)
 prf_answ=$(cat perfect_cache/answ/$i.answ)
 if [ $arc_answ -gt $prf_answ ]
 then
-echo "Test $i: ARC"
+echo "Test $i: ARC ($arc_answ > $prf_answ)"
 else
-echo "Test $i: perfect_cache"
+echo "Test $i: perfect_cache ($prf_answ > $arc_answ)"
 fi
 done
