5b1e47b241d8084194835c7c23777d35940a69e6
yanelox <hokeygo@gmail.com>
2022-02-27 14:45:53 +0300

    delete trash

--- Matrix/gtest/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/yanelox/DISK1/linux_work/ilab_2year/Matrix/gtest/build/_deps/googletest-src/googletest
-# Build directory: /media/yanelox/DISK1/linux_work/ilab_2year/Matrix/gtest/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
